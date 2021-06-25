7388b8e2182db6155cf711f40e0d2cc3f8b8ac79
ducthaidev@gmail.com <Thai.1201>
2021-06-25 23:46:05 +0700

    setting something

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import createSagaMiddleware from "redux-saga"
+import "./index.css"
+import { applyMiddleware, compose, createStore } from "redux"
+import { Provider } from "react-redux"
+import App from "./App"
+import rootReducer from "./reducers"
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(sagaMiddleware))
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+)
