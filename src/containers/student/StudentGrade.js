9cf094959ca9d84dfcf5d7bee6615507e7cd98d7
ducth <ducthaidev@gmail.com>
2021-07-21 10:54:45 +0700

    add grade show api client

--- src/containers/student/StudentGrade.js
+++ src/containers/student/StudentGrade.js
@@ -1,8 +1,69 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
+import {
+  Add,
+  ArrowUpward,
+  Cancel,
+  Check,
+  Clear,
+  Delete,
+  Edit as EditIcon,
+  FilterList,
+  FirstPage,
+  LastPage,
+  NavigateBefore,
+  NavigateNext,
+  Replay,
+  Search,
+} from "@material-ui/icons"
+import MaterialTable from "material-table"
 import useAuthRequest from "../../helpers/useAuthRequest"
 import BasicSelect from "../../components/form/BasicSelect/BasicSelect"
 import BasicOption from "../../components/form/BasicOption/BasicOption"
+
+const columns = [
+  { title: "Môn", field: "subject.name" },
+  { title: "Điểm", field: "grade" },
+]
+
+const localization = {
+  header: {
+    actions: "",
+  },
+  toolbar: {
+    searchPlaceholder: "Tìm Kiếm",
+  },
+  pagination: {
+    labelRowsSelect: "Dòng",
+    labelDisplayedRows: "{from}-{to} của {count}",
+  },
+  body: {
+    emptyDataSourceMessage: "Không Tồn Tại Bản Ghi Nào",
+    editTooltip: "Sửa",
+    editRow: {
+      cancelTooltip: "Huỷ bỏ",
+      saveTooltip: "Tiếp tục",
+    },
+  },
+}
+
+const icons = {
+  Delete,
+  Add,
+  Check,
+  Cancel,
+  Clear,
+  Edit: EditIcon,
+  Search,
+  ResetSearch: Clear,
+  FirstPage,
+  LastPage,
+  NextPage: NavigateNext,
+  PreviousPage: NavigateBefore,
+  Filter: FilterList,
+  Retry: Replay,
+  SortArrow: ArrowUpward,
+}
 
 function StudentGrade() {
   const { student } = useSelector((state) => state.authentication.account)
@@ -12,6 +73,7 @@ function StudentGrade() {
   const onSemesterInputChange = (e) => {
     setSemesterInput(e.target.value)
   }
+
   const [gradeTypeInput, setGradeTypeInput] = useState("0")
   const onGradeTypeInputChange = (e) => {
     setGradeTypeInput(e.target.value)
@@ -20,6 +82,13 @@ function StudentGrade() {
   const [semesters, setSemesters] = useState()
   const fetchSemestersWithSession = (sessionId) =>
     authRequest.get(`/api/semesters?sessionId=${sessionId}`)
+
+  // eslint-disable-next-line no-unused-vars
+  const [grades, setGrades] = useState([])
+  const fetchGrades = (gradeType, semesterId, studentId) =>
+    authRequest.get(
+      `/api/studies/filter?studentId=${studentId}&gradeType=${gradeType}&semesterId=${semesterId}`
+    )
 
   useEffect(() => {
     document.title = "Xem Điểm Thi"
@@ -32,13 +101,30 @@ function StudentGrade() {
       )
       setSemesters(fetchedSemesters.data)
       if (fetchedSemesters.data.length > 0) {
-        setSemesterInput(fetchedSemesters.data[0].id)
+        setSemesterInput(fetchedSemesters.data[0].id.toString())
       }
     } catch (e) {
       // eslint-disable-next-line no-console
       console.log(e)
     }
   }, [])
+
+  useEffect(async () => {
+    try {
+      if (semesterInput.length > 0 && gradeTypeInput.length > 0) {
+        const fetchedGrades = await fetchGrades(
+          gradeTypeInput,
+          semesterInput,
+          student.id
+        )
+        setGrades(fetchedGrades.data)
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err)
+    }
+  }, [semesterInput, gradeTypeInput])
+
   return (
     <>
       <h1>Xem Điểm Thi</h1>
@@ -69,6 +155,14 @@ function StudentGrade() {
           content="name"
         />
       </BasicSelect>
+
+      <MaterialTable
+        title="Thực Hành"
+        icons={icons}
+        columns={columns}
+        localization={localization}
+        data={grades}
+      />
     </>
   )
 }
